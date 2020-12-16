ba4ed7548d57a4b315d7e90cc2f9dcac44aa1450
Trenten grede <Trenten.grede@gmail.com>
2020-12-16 01:57:37 -0600

    finished with all 22 tests passing

--- index.js
+++ index.js
@@ -42,8 +42,17 @@ function Airplane(name) {
  function Person(name, age) {
    this.name = name;
    this.age = age;
-    
-  }
+   this.stomach = []; 
+   }
+  Person.prototype.eat = function(str){
+    this.stomach.push(str)
+ };
+ Person.prototype.poop = function(){
+   this.stomach.length = 0;
+ }
+ Person.prototype.toString = function(){
+   return this.name + ", " + this.age;
+ }
  
  
 
@@ -65,8 +74,14 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  };
+  Car.prototype.fill = function(gallons){
+    this.tank += gallons;
   }
   
   
@@ -77,8 +92,13 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
+  }
+  Baby.prototype = new Person();
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`
   }
  
   
